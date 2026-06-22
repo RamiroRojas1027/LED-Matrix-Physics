993101cb06ebb65ee896708d022b23c9f82cb9da
RamiroRojas1027 <ramiro.rojas226@gmail.com>
2026-06-22 18:22:08 -0400

    Added shutdownAll() function

--- LED_Matrix_Physics/LED_Matrix_Physics.ino
+++ LED_Matrix_Physics/LED_Matrix_Physics.ino
@@ -5,14 +5,8 @@ LedControl lc = LedControl(12, 11, 10, 4);
 
 void setup() {
   // Serial.begin(115200);
-  lc.shutdown(0, false);
-  lc.shutdown(1, false);
-  lc.shutdown(2, false);
-  lc.shutdown(3, false);
-  // lc.setIntensity(0, 15);
-  // lc.setIntensity(1, 15);
-  // lc.setIntensity(2, 15);
-  // lc.setIntensity(3, 15);
+  shutdownAll(false);
+  setIntensityAll(5);
   lc.clearDisplay(0);
   for(char i = 0; i < 4; i++) {
     for(char j = 0; j < 8; j++) {
@@ -25,6 +19,12 @@ void loop() {
   for(char i = 0; i < 16; i++) {
     setIntensityAll(i);
     delay(1000);
+  }
+}
+
+void shutdownAll(bool status) {
+  for(char i = 0; i < 4; i++) {
+    lc.shutdown(i, status);
   }
 }
 
