1dfbdfd1e8b90bb58929a7629388a4bea4290d26
RamiroRojas1027 <ramiro.rojas226@gmail.com>
2026-06-22 19:45:29 -0400

    Added inverted display functionality

--- LED_Matrix_Physics/LED_Matrix_Physics.ino
+++ LED_Matrix_Physics/LED_Matrix_Physics.ino
@@ -3,6 +3,7 @@
 // General matrix variables
 LedControl lc = LedControl(12, 11, 10, 4);   // DIN, CLK, CS, numDevices
 const unsigned char WIDTH = 32, HEIGHT = 8;
+bool invertedDisplay = false;
 
 // Box simulation variables
 unsigned char boxX = 0, boxY = 0, boxSize = 5; bool boxFill = false;
@@ -24,6 +25,8 @@ void loop() {
 }
 
 void drawBox(bool state) {
+  if(invertedDisplay) state = !state;
+  
   if(boxX < 0) boxX = 0;
   else if(boxX > WIDTH - boxSize) boxX = WIDTH - boxSize;
   if(boxY < 0) boxY = 0;
@@ -69,5 +72,10 @@ void setIntensityAll(char intensity) {
 void clearDisplayAll() {
   for(char i = 0; i < 4; i++) {
     lc.clearDisplay(i);
+    if(invertedDisplay) {
+      for(char j = 0; j < 8; j++) {
+        lc.setRow(i, j, 0b11111111);
+      }
+    }
   }
 }
